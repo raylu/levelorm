cd25b156185a1e1d9c13793035f7bdd5bcc4256f
raylu <lurayl@gmail.com>
2017-09-09 03:20:39 -0700

    add a bunch of support files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='levelorm',
+		version='0.1',
+		description='python3.6+ ORM for leveldb using plyvel',
+		url='http://github.com/raylu/levelorm',
+		author='raylu',
+		author_email='raylu@users.noreply.github.com',
+		packages=['levelorm'],
+		python_requires='>=3.6', # https://docs.python.org/3/reference/datamodel.html#object.__init_subclass__
+		install_requires=[
+			'plyvel',
+		],
+		test_suite='tests',
+		zip_safe=True)
